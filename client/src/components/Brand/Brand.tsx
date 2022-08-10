de8e5131de34b565393a80e534c180212d338138
wils446 <wilsonlimmmm@gmail.com>
2022-08-11 01:10:12 +0700

    add Brand component

---
+++ client/src/components/Brand/Brand.tsx
@@ -0,0 +1,7 @@
+import styles from "./Brand.module.css";
+
+const Brand = () => {
+    return <h1 className={styles.brand}>Battle Math</h1>;
+};
+
+export default Brand;
---
+++ client/src/components/index.ts
@@ -0,0 +1,5 @@
+import Brand from "./Brand";
+import PageHeader from "./PageHeader";
+import ReadyBoard from "./ReadyBoard";
+
+export { Brand, PageHeader, ReadyBoard };
