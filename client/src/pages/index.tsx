89a4625397807f3d4d06c3906a7667392943dc72
wils446 <wilsonlimmmm@gmail.com>
2022-08-11 01:12:08 +0700

    add Home layout

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,8 +1,52 @@
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { ChangeEventHandler, useState } from "react";
+import { Brand, PageHeader } from "../components";
+import useLocalStorage from "../hooks/useLocalStorage";
 import styles from "../styles/Home.module.css";
+import getRandomString from "../utils/getRandomString";
 
 const Home: NextPage = () => {
-    return <div className={styles.container}></div>;
+    const router = useRouter();
+    const [roomCode, setRoomCode] = useState("");
+    const [code, setCode] = useLocalStorage("roomCode");
+
+    const roomCodeInputOnChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+        //max input length is 10
+        if (event.target.value.length > 10) return;
+        setRoomCode(event.target.value);
+    };
+
+    const joinRoom = () => {
+        setCode(roomCode);
+        router.push("/lobby");
+    };
+    const createRoom = () => {
+        const roomCode = getRandomString();
+        setCode(roomCode);
+        router.push("/lobby");
+    };
+
+    const onPressEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter") joinRoom();
+    };
+
+    return (
+        <>
+            <PageHeader title="Battle Math!" />
+            <div className={styles["layout-container"]}>
+                <Brand />
+                <div className={styles.content}>
+                    <h1>Enter Room Code : </h1>
+                    <input type="text" onChange={roomCodeInputOnChange} onKeyDown={onPressEnter} value={roomCode} />
+                    <br />
+                    <button onClick={joinRoom}>Join Room</button>
+                    <br />
+                    <button onClick={createRoom}>Create Room</button>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Home;
